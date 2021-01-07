9d4dd10354b53ae66d095eb10d6d60537308260a
Parker Burchett <parkerburchett@gmail.com>
2021-01-06 19:44:26 -0800

    Added descriptions of luck distributions

--- Luck Simulator/Luck Simulator.py
+++ Luck Simulator/Luck Simulator.py
@@ -16,15 +16,19 @@ class Person():
 def get_score(a_person):
         return int(.05*a_person.luck +.95*a_person.competence)
 
-competence_scores = np.random.normal(loc=50, scale=10, size=10000)
+sample_size =100000
 
-luck_scores = np.random.randint(0,100,100000)
+competence_scores = np.random.normal(loc=50, scale=10, size=sample_size)
+
+luck_scores = np.random.randint(0,100,sample_size)
+
+
 
 # this is bad syntax but it does not matter. It works and it is just to prove a concept
 persons_score_tuple =[]
 persons = []
 
-for i in range(10000):
+for i in range(sample_size):
     a_person = Person(competence_scores[i],luck_scores[i])
     score = a_person.get_score()
     persons.append(a_person)
@@ -32,16 +36,28 @@ for i in range(10000):
 
 
 persons.sort(key=get_score, reverse=True)
-
-
+lucky_10 = []
+comp_10 =[]
+num_better_50_percent_luck = 0
+num_better_90_percent_luck =0
+print('When you have {} people trying to get into 10 slots:'.format(sample_size))
 for i in persons[:10]:
     luck = i.luck
+    if luck >=50:
+        num_better_50_percent_luck +=1
+    if luck >=90:
+        num_better_90_percent_luck +=1
     comp = int(i.competence)
     score = i.get_score()
-    print('Competence {} Luck {} Weighted Score {}'.format(comp,luck,score))
+    lucky_10.append(luck)
+    comp_10.append(comp)
+    #print('Competence {} Luck {} Weighted Score {}'.format(comp,luck,score))
 
-
-
+avg_std_above_mean = round((np.average(comp_10)-50) /10, 2) # this is a simplified formula for standard deviation.
+print('You need to have an average competence score of {} Standard deviations above the mean.'.format(avg_std_above_mean))
+print('And be more lucky that {}% of your peers'.format(np.average(lucky_10)))
+print('Amoung those 10\n{} had better than 50% luck \n{} had better than 90% luck'.format(num_better_50_percent_luck
+                                                                                            ,num_better_90_percent_luck))
 print('fin')
 
 
